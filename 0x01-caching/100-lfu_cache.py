078669de3852990deeb0f6ea4016b29de1b502bd
VicvekSr <Olamideoso65@gmail.com>
2023-01-28 00:01:43 +0000

    add an LFU caching system

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python3
-""" LFU Caching module """
-
+"""Class representation of LFU caching
+"""
 from base_caching import BaseCaching
-from collections import Counter
+from collections import OrderedDict
+import operator
 
 
 class LFUCache(BaseCaching):
-    """ LFU caching system """
+    """LFUCache that inherits from BaseCaching
+    """
     def __init__(self):
-        """ init method for LFU caching """
+        """Initialize LFUCache
+        """
         super().__init__()
-        self.counter = Counter()
+        self.freq = OrderedDict()
+        self.sorted = None
 
     def put(self, key, item):
-        """Add an item to the cache"""
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            min_freq = min(self.counter.values())
-            lfu_keys = [k for k, v in self.counter.items() if v == min_freq]
-            lru_key = min(lfu_keys, key=lambda k: self.cache_data[k][1])
-            print("DISCARD: {}".format(lru_key))
-            del self.cache_data[lru_key]
-            del self.counter[lru_key]
-        self.counter[key] += 1
-        self.cache_data[key] = (item, self.counter[key])
+        """Assign key and item to the cache system
+        """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.freq:
+            self.sorted = sorted(self.freq.items(), key=operator.itemgetter(1))
+            discard = self.sorted.pop(0)[0]
+            del self.freq[discard]
+            del self.cache_data[discard]
+            print("DISCARD: {}".format(discard))
+
+        if key and item:
+            val = 0
+            if key in self.cache_data:
+                val = self.freq[key]
+                del self.freq[key]
+
+            self.freq[key] = val + 1
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item from the cache"""
-        if key is None or key not in self.cache_data:
+        """Fetch data from the cache system with key
+        """
+        if not key or key not in self.cache_data:
             return None
-        self.counter[key] += 1
-        self.cache_data[key] = (self.cache_data[key][0], self.counter[key])
-        return self.cache_data[key][0]
+
+        val = 0
+        if key in self.freq.keys():
+            val = self.freq[key]
+            del self.freq[key]
+
+        self.freq[key] = val + 1
+        return self.cache_data[key]
