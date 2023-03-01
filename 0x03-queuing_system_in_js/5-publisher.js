0298594affc4178667d21ec9414c61a0cb5e7df9
VicvekSr <Olamideoso65@gmail.com>
2023-03-01 05:55:14 +0000

    redis config

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,7 +1,7 @@
 import redis from 'redis';
 import { createClient } from 'redis';
 
-client = createClient();
+const client = createClient();
 
 client.on('connect', () => {
     console.log('Redis client connected to the server')
