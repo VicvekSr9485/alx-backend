b5d51c4e19a9e49e32d3459c22290b59821cb8ed
VicvekSr <Olamideoso65@gmail.com>
2023-03-01 06:22:57 +0000

    add job processor

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -4,3 +4,9 @@ const queue = kue.createQueue();
 function sendNotification(phoneNumber, message) {
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
+
+queue.process('push_notification_code', (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
